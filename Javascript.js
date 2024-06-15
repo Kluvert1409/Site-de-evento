b8514691615bd924a59382a214c9984dfcfbf51f
Kluvert1409 <kluvertgabriel07@gmail.com>
2024-06-15 16:34:57 -0300

    commit

---
+++ Javascript.js
@@ -0,0 +1,18 @@
+function ShowSlipknot()
+{
+    window.location.assign("ShowSlipknot.html")
+}
+
+function ShowLinkinPark()
+{
+    window.location.assign("ShowLinkinPark.html")
+}
+
+function ShowGojira()
+{
+    window.location.assign("ShowGojira.html")
+}
+
+function botaoCadastrar() {
+alert("Cadastro concluído com sucesso")
+}
